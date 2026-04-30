846b1e71707c2fc9f20e471c55eadf11ab035680
Рибка <ostaprybak395@gmail.com>
2026-04-30 22:33:02 +0300

    small fix

--- labs/lab06/Rybka/main.cpp
+++ labs/lab06/Rybka/main.cpp
@@ -8,37 +8,37 @@
 using namespace std;
 
 class Student {
-private:
+	private:
 	string name;
 	string surname;
 	string group;
 	int age;
 public:
 	Student() = default;
-  friend istream& operator>>(istream& is, Student& s) {
-      return is >> s.name >> s.surname >> s.group >> s.age;
-  }
-  friend ostream& operator<<(ostream& os, const Student& q) {
-      return os << q.name << ' ' << q.surname << ' '  << q.group << ' ' << q.age;
-  }
+	friend istream& operator>>(istream& is, Student& s) {
+	  return is >> s.name >> s.surname >> s.group >> s.age;
+	}
+	friend ostream& operator<<(ostream& os, const Student& q) {
+	  return os << q.name << ' ' << q.surname << ' '  << q.group << ' ' << q.age;
+	}
 };
 
 int main() {
-    ifstream file("res.txt");
-    vector<Student> students;
-    if(!file.is_open()) {
-        cerr << "file cant be opened\n";
-        return 0;
+	ifstream file("students.txt");
+	vector<Student> students;
+	if(!file.is_open()) {
+		cerr << "file cant be opened\n";
+		return 0;
 	}
-    copy(istream_iterator<Student>(file), istream_iterator<Student>(), back_inserter(students));
-	  file.close(); 
-    if (students.empty()) {
-        cerr << "empty file\n";
-        return 0;
-    }
-    else {
+	copy(istream_iterator<Student>(file), istream_iterator<Student>(), back_inserter(students));
+	file.close(); 
+	if (students.empty()) {
+		cerr << "empty file\n";
+		return 0;
+	}
+	else {
 		cout << "Students:\n";
 		copy(students.begin(), students.end(), ostream_iterator<Student>(cout, "\n"));
-	  }
-    return 0;
+	}
+	return 0;
 }
